3826bc060cf40575b8642d699b9e48989cb04c72
Akshat Rawat <axr9972@rit.edu>
2021-11-10 17:37:54 -0500

    updated to domomaker D

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,8 +6,21 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
+// const signupPage = (req, res) => {
+//  res.render('signup', { csrfToken: req.csrfToken() });
+// };
+
+// replaced by the following
+
+const getToken = (request, response) => {
+  const req = request;
+  const res = response;
+
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
+
+  res.json(csrfJSON);
 };
 
 const logout = (req, res) => {
@@ -84,5 +97,6 @@ const signup = (request, response) => {
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
+// module.exports.signupPage = signupPage;
+module.exports.getToken = getToken;
 module.exports.signup = signup;
